379dd52599a3f56628400ca80744d16e0cd3ff6a
rosalind1lee <rlee90@chicagobooth.edu>
2021-04-14 20:02:48 +0000

    count_the and black_jack exercises

--- black_jack.rb
+++ black_jack.rb
@@ -8,3 +8,18 @@
 # For example, given a 11 and 13 as input, the 11 should be 'converted' into a 1 so the total sum will be 14.
 
 p "Enter two number separated by spaces:"
+nums = gets.chomp
+arrnums = nums.split.map(&:to_i)
+#p arrnums
+
+if arrnums[0] + arrnums[1] > 21
+  if arrnums[0] == 11
+    p 1 + arrnums[1]
+  elsif  arrnums[1] == 11
+    p 1 + arrnums[0]
+  else
+    p 0
+  end
+else
+  p arrnums[0] + arrnums[1]
+end
--- count_the.rb
+++ count_the.rb
@@ -4,3 +4,17 @@
 #   and finally prints, "'the' appeared x times", where x is an Integer
 
 p "Enter a sentence:"
+sentence = gets.chomp
+sentence = sentence.gsub(/[^0-9a-z ]/i, "")
+p sentence
+words = sentence.split
+
+i = 0
+count = 0
+while i < words.length
+  if words[i] == "the"
+    count = count + 1
+  end
+  i = i + 1
+end
+p "'the' appeared #{count} times"
